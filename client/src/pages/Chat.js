93415dbd566f2ff2a13fcf805bbc4e7443908706
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-11-12 18:03:37 +0100

    doing css & logout + toggle function for chat-page

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,21 +1,74 @@
-import React from "react";
-import './Chat.css'
+import React, { useState, useEffect } from "react";
+import './Chat.css';
+import { useUser } from '../components/Usercontext';
+import { useNavigate } from "react-router-dom";
 
 function Chat() {
+  const [leftContainerVisible, setLeftContainerVisible] = useState(true);
+  const { loggedIn, logout } = useUser();
+  const navigate = useNavigate();
 
-    return (
-      <div>
-        <div className="top-container">
+  const handleToggle = () => {
+    setLeftContainerVisible(!leftContainerVisible);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setLeftContainerVisible(false);
+      } else {
+        setLeftContainerVisible(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); 
+
+  const handlelogout = () => {
+    logout();
+    setTimeout(() => {
+      navigate("/login");
+    }, 10)
+    console.log("successfully logged out")
+  };
+
+  return (
+    <div className="container">
+      {leftContainerVisible && (
+        <div className="left-container">
           <div className="title-container">
             <p className="logo">Chat App</p>
           </div>
-          <div className="logout-container">
-            <button>sdfs</button>
-            <h1>dsfafaf</h1>
+          <div className="user-container">
+            <h1>skdfjsld</h1>
           </div>
         </div>
+      )}
+      <div className="right-container">
+        <div className="logout-container">
+          {window.innerWidth <= 768 && (
+            <button onClick={handleToggle}>☰</button>
+          )}
+          <p>
+            <button className="logout-btn" onClick={handlelogout}>logout</button>
+          </p>
+        </div>
+        <div className="chat-container">
+          <h3>chat</h3>
+        </div>
+        <div className="input-container">
+          <input class="msg-input" name="text" placeholder="Type something..." type="search"/>
+          <button>Send</button>
+        </div>
       </div>
-    );
-  }
-  
-  export default Chat;
+    </div>
+  );
+}
+
+export default Chat;
