6de3acb1f90b52489651c1dba22df900c80d32dc
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-11-21 17:45:27 +0100

    installed socket.io

--- server/server.js
+++ server/server.js
@@ -1,28 +1,32 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { Server } = require("socket.io");
 
 dotenv.config();
+app.use(bodyParser.json());
+app.use(cors());
+
+const server = http.createServer(app);
 
 const port = 3000;
 const saltRound = 10;
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST" ],
+  },
+})
 
-  app.use(bodyParser.json());
-
-  app.use(cors({
-    origin : ["http://localhost:3001"],
-    methods : ["GET", "POST", "DELETE"],
-    credentials: true,
-  }))
-
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  server.listen(3000, () => {
+    console.log("server running on port 3000")
+  })
 
   const pool = mysql.createPool({
     host: process.env.DB_HOST,
