3929e6e6ad0719d6df0f2259a760eb63f6fddae4
sujan_Saravana <sujan.saravana@lernende.bbw>
2023-11-22 19:12:18 +0100

    created new tables

--- database/database.sql
+++ database/database.sql
@@ -14,14 +14,39 @@ VALUES
 ('bob', 'bob@gmail.com', '1234'),
 ('john', 'john@gmai.com', '1122');
 
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(255) NOT NULL,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+INSERT INTO rooms (name)
+VALUES
+('bob');
+
 CREATE TABLE messages (
   id INT AUTO_INCREMENT PRIMARY KEY,
   senderId INT,
   receiverId INT,
   content TEXT,
-  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (senderId) REFERENCES users(id),
+  FOREIGN KEY (receiverId) REFERENCES users(id)
 );
 
 INSERT INTO messages (senderId, receiverId, content)
 VALUES
 ('1', '2', 'Hello, my name is john');
+
+CREATE TABLE user_room (
+  userId INT,
+  roomId INT,
+  PRIMARY KEY (userId, roomId),
+  FOREIGN KEY (userId) REFERENCES users(id),
+  FOREIGN KEY (roomId) REFERENCES rooms(id)
+);
+
+INSERT INTO user_room (userId, roomId)
+VALUES
+('1', '1'),
+('2', '1');
